771e3fc8c2165980292c931e44ab1e215fda5244
itc-s21025 <s21025@std.it-college.ac.jp>
2021-12-05 09:12:02 +0000

    temprate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import "./App.css"
+import Header from "./Header.js"
 
-function App() {
+
+const bookname = '四畳半神話大系'
+const uri = `https://www.googleapis.com/books/v1/volumes?q=intitle:${bookname}&maxResults=1`
+
+const App = props => {
+  const [data, setData] = useState([])
+  useEffect(
+    () =>
+      window
+        .fetch(uri)
+        .then(res => res.json())
+        .then(json => json.items)
+        .then(data => setData(data)),
+    []
+  )
+
+
+
+  const a = data.volumeInfo
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <Header text="書籍検索" />
+      <div className="item">
+        <ul>
+          <div className="items">
+            {data.map(v => (
+              <ol key={v.volumeInfo}>作品名：   {v.volumeInfo.title}</ol>
+            ))}
+          </div>
 
-export default App;
+          <div className="items">
+            {data.map(v => (
+              <ol key={v.voulumeInfo}>著者：{v.volumeInfo.authors}</ol>
+            ))}
+          </div>
+
+          <div className="items2">
+            <h2>あらすじ</h2>
+            {data.map(v => (
+              <p key={v.imageLinks}>{v.volumeInfo.description}</p>
+            ))}
+          </div>
+
+
+        </ul>
+      </div>
+    </>
+  )
+}
+export default App
