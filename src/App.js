152dead2fa6731874c19bbde5137655383750c10
itc-s21025 <s21025@std.it-college.ac.jp>
2021-12-09 00:19:52 +0000

    searchok

--- src/App.js
+++ src/App.js
@@ -5,55 +5,51 @@ import Header from "./Header.js"
 
 const App = () => {
   const [data, setData] = useState([])
+
   const search = searchValue => {
-    setLoading(true);
-    setErrorMessage(null);
-
-    useEffect(
-      () =>
-        fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle:${searchValue}&maxResults=1`)
-          .then(response => response.json())
-          .then(json => json.items)
-          .then(data => setData(data)),
-      []
-    )
-
-    const bookname = 'React'
-    const uri = `https://www.googleapis.com/books/v1/volumes?q=intitle:${bookdata}&maxResults=1`
-
-
-
-
-    return (
-      <>
-        <Header text="書籍検索" />
-        <Search search={search} />
-        <div className="item">
-          <ul>
-            <div className="items">
-              {data.map(v => (
-                <ol key={v.volumeInfo}>作品名：   {v.volumeInfo.title}</ol>
-              ))}
-            </div>
-
-            <div className="items">
-              {data.map(v => (
-                <ol key={v.voulumeInfo}>著者：{v.volumeInfo.authors}</ol>
-              ))}
-            </div>
-
-            <div className="items2">
-              <h2>あらすじ</h2>
-              {data.map(v => (
-                <p key={v.imageLinks}>{v.volumeInfo.description}</p>
-              ))}
-            </div>
-
-
-          </ul>
-        </div>
-      </>
-    )
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle:${searchValue}&maxResults=1`)
+      .then(response => response.json())
+      .then(json => json.items)
+      .then(data => setData(data));
   }
+
+
+
+
+
+
+
+
+  return (
+    <>
+      <Header text="書籍検索" />
+      <Search search={search} />
+      <div className="item">
+        <ul>
+          <div className="items">
+            {data.map(v => (
+              <ol key={v.volumeInfo}>作品名：   {v.volumeInfo.title}</ol>
+            ))}
+          </div>
+
+          <div className="items">
+            {data.map(v => (
+              <ol key={v.voulumeInfo}>著者：{v.volumeInfo.authors}</ol>
+            ))}
+          </div>
+
+          <div className="items2">
+            <h2>あらすじ</h2>
+            {data.map(v => (
+              <p key={v.imageLinks}>{v.volumeInfo.description}</p>
+            ))}
+          </div>
+
+
+        </ul>
+      </div>
+    </>
+  )
 }
+
 export default App
